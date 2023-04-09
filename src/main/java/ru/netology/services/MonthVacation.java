1b7f3901727242f8d2f4ac1f1863a6aabf0c765c
perova test <av_perova@mail.com>
2023-04-09 20:54:25 +0700

    Reformate code

--- src/main/java/ru/netology/services/MonthVacation.java
+++ src/main/java/ru/netology/services/MonthVacation.java
@@ -1,15 +1,16 @@
 package ru.netology.services;
+
 public class MonthVacation {
 
-    public int calcMonthVacation(int income, int expenses, int threshhold){
+    public int calcMonthVacation(int income, int expenses, int threshhold) {
         int count = 0;
         int money = 0;
-        for (int month = 1; month < 12; month++){
-            if (money >= threshhold){
+        for (int month = 1; month < 12; month++) {
+            if (money >= threshhold) {
                 count++;
                 money = money - expenses;
                 money = money / 3;
-            }else{
+            } else {
                 money = money + income - expenses;
             }
         }
